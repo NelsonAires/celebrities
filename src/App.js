046bedb95808aaaf31ac7481a1efec50a81d7500
Nelson Aires <nelsonaires@Nelsons-MacBook-Pro.local>
2019-05-14 12:24:33 +0100

    1st push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        celebrities:[]
+      };
+      // const {celebrities} = this.celebrities;
+    }
+    // const {celebrities} = this.props;
+      componentDidMount() {
+        axios.get('https://api.themoviedb.org/3/person/popular?page=5&api_key=d7d00236f3a0891b3c07fd98f6f189cd')
+          .then((res) => { 
+            console.log(res.data.results)      
+            this.setState({celebrities:res.data.results});
+          })
+        }
+
+  render() {
+    return (
+    //   <div className={this.state.celebrities}>
+    //   This will be hidden if you set <tt>props.shouldHide</tt> 
+    //   to something truthy.
+    // </div>
+    <div className="celebs">
+      <h1>Celebs</h1>
+    
+  
+      
+      {this.state.celebrities.map(celebrities => 
+      <ul key={celebrities.id}> 
+        <img src={"https://image.tmdb.org/t/p/w185"+celebrities.profile_path} class="profPicture" alt="profile"/>
+      {celebrities.name}</ul>)}
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+
